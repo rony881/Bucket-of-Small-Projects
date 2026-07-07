834e894894a9d7204f0c6ab6ea2927ce7f5243e6
Rony <ronybiswasinnovative@gmail.com>
2026-07-07 22:55:03 +0600

    Changed name: main.py -> bouncing_ball.py

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -3,14 +3,12 @@ import sys
 
 pygame.init()
 
-# Screen setup
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gravity Ball")
 
 clock = pygame.time.Clock()
 
-# Ball properties
 ball_x = 400
 ball_y = 100
 ball_radius = 25
@@ -25,21 +23,17 @@ running = True
 while running:
     clock.tick(60)
 
-    # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Gravity
     velocity_y += gravity
     ball_y += velocity_y
 
-    # Collision with floor
     if ball_y >= floor:
         ball_y = floor
         velocity_y *= bounce
 
-    # making a ball
     screen.fill((30, 30, 30))
     pygame.draw.circle(screen, (255, 100, 100), (int(ball_x), int(ball_y)), ball_radius)
 
